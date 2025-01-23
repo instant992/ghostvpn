35df08c36cfeb0cf78c691dcee837aae7f41c1a7
Женя <instant99@mail.ru>
2025-01-24 00:21:08 +0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>👻ghostVPN</span>,
+  logo: <span>👻<b>ghostVPN</b></span>,
   chat: {
     link: 'https://t.me/vpnghostbot',
   },
