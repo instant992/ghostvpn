f0d1a7d4a76b25d2856cfd5fbf4750e8cec94a92
Женя <instant99@mail.ru>
2025-01-25 20:47:32 +0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>👻<b>ghostVPN</b></span>,
+  logo-telegram: <span>👻<b>ghostVPN</b></span>,
   chat: {
     link: 'https://t.me/vpnghostbot',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: '#ghostVPN - 2023-2025. Сюда и так никто не посмотрит. 🦊',
+    text: '#ghostVPN © 2023-2025. Сюда и так никто не посмотрит. 🦊',
   },
 }
 
